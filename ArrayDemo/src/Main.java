3bf53b124381cd803cf1c53a68ac7aa026ad3573
Fredrik Terent <fredrik.terent@xlent.se>
2021-10-18 13:01:29 +0200

    Demo

---
+++ ArrayDemo/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//		String[] elever=new String[3];
+//		elever[0]="Kalle";
+//		elever[1]="Lisa";
+//		elever[2]="Kim";
+		
+		String[] elever= {"Kalle","Lisa",
+				"Kim"};
+
+		for (String elevNamn:elever) {
+			System.out.println(elevNamn);
+		}
+		
+//		for (int i=0; i< elever.length; i++) {
+//			System.out.println("Pos: "+ i + " " + elever[i]);
+//		}
+
+	}
+}
